aa331fb92d33ca3966f926e410609fd7eb3fc9a2
Evan <evanantunanocoding@gmail.com>
2019-08-21 14:44:30 -0500

    answered question about 4 principles of this keyword

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Whether the function is called by "new"
+* 2. If the function is called by "call()" "apply()" or "bind()"
+* 3. If the function is being called to act as a method
+* 4. if the function is called in the global scope (strict mode:enabled = "undefined")
 *
 * write out a code example of each explanation above
 */
